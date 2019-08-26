15dee93573377ffbb4b33cbfa01182c3b4f70e56
zwl <7330385+esvhd@users.noreply.github.com>
2019-08-26 23:17:50 +0100

    comments

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -10,11 +10,14 @@ def test_log_uniform_draw():
     size = 1000
     ns = mls.log_uniform_draw(size, a=a, b=b)
 
-    # KS test null hypothesis is the sames come from the specified distribution
+    # KS test null hypothesis is the data come from the specified distribution
+    # therefore, failing to reject the null means confirms that the null
+    # is true.
     ks = kstest(
         rvs=np.log(ns), cdf="uniform", args=(np.log(a), np.log(b / a)), N=size
     )
 
     # assert that the null cannot be rejected, i.e. data is uniform.
     print(f"K-S test p-value: {ks.pvalue:.5e}")
-    assert ks.pvalue > 0.01
+    # assert failing to reject null
+    assert ks.pvalue > 0.05
