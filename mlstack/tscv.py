10bb47e38886e4dac42e540d302c26c2994a2e1a
esvhd <weiliang.zhang@gmail.com>
2019-08-26 23:01:23 +0100

    format and wide to long util func

--- mlstack/tscv.py
+++ mlstack/tscv.py
@@ -3,11 +3,53 @@ Time series CV tools
 """
 import pandas as pd
 import warnings
-from sklearn import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit
 
 
-def time_series_embargo_split(X, embargo_size: int, n_splits: int,
-                              max_train_size=None):
+def wide_to_long(
+    df: pd.DataFrame,
+    id_var: str = "index",
+    var_name: str = "name",
+    value_name: str = "value",
+) -> pd.DataFrame:
+    """Convert wide format dataframe to long format. Result dataframe is sorted
+    by var_name which contain original df's column names and by original df's
+    index as field names.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        input wide data frame
+    id_var : str, optional
+        field name, by default "index", i.e. using input df's index
+    var_name : str, optional
+        variable name, using column names in the input df, by default "name"
+    value_name : str, optional
+        Value column name in the output long format df, by default "value"
+
+    Returns
+    -------
+    pd.DataFrame
+        long format data frame with NaNs dropped.
+    """
+    df = (
+        df
+        # reset index so date becomes a column
+        .reset_index().melt(
+            id_vars=id_var, var_name=var_name, value_name="value"
+        )
+        # remove NaN not aligned in wide format, e.g. due to holidays
+        .dropna(axis=0)
+    )
+    df.sort_values([var_name, id_var], inplace=True)
+    # reindex
+    df.index = [x for x in range(len(df))]
+    return df
+
+
+def time_series_embargo_split(
+    X, embargo_size: int, n_splits: int, max_train_size=None
+):
     """Generate time series CV with embargo, i.e. a gap exists between train
     and test data.
 
@@ -26,7 +68,7 @@ def time_series_embargo_split(X, embargo_size: int, n_splits: int,
     ------
     tuple of indices of train and test data
     """
-    assert(embargo_size >= 0), 'embargo_size must be non negetive.'
+    assert embargo_size >= 0, "embargo_size must be non negetive."
 
     # split with time series split
     tscv = TimeSeriesSplit(n_splits, max_train_size=max_train_size)
@@ -37,19 +79,23 @@ def time_series_embargo_split(X, embargo_size: int, n_splits: int,
 
     # for each pair, chop off the last few samples in training
     for train_idx, test_idx in base_cv:
-        assert(len(train_idx) > embargo_size)
+        assert len(train_idx) > embargo_size
         if len(train_idx) < 2 * embargo_size:
-            warnings.warn(f'Training size {len(train_idx)} < 2x ' +
-                          f'Embargo size {embargo_size}')
+            warnings.warn(
+                f"Training size {len(train_idx)} < 2x "
+                + f"Embargo size {embargo_size}"
+            )
         yield train_idx[:-embargo_size], test_idx
 
 
-def long_format_ts_embargo_split(long_df: pd.DataFrame,
-                                 time_col: str,
-                                 embargo_size: int,
-                                 n_splits: int,
-                                 sort_time_col=True,
-                                 debug=False):
+def long_format_ts_embargo_split(
+    long_df: pd.DataFrame,
+    time_col: str,
+    embargo_size: int,
+    n_splits: int,
+    sort_time_col=True,
+    debug=False,
+):
     """Generate time series embargo split indices for a long format data frame.
     A column representing time must exist.
 
@@ -74,7 +120,7 @@ def long_format_ts_embargo_split(long_df: pd.DataFrame,
     tuple of train and test indices
     """
     # convert from long to wide format, use X to hold dates
-    assert(time_col in long_df.columns), f'{time_col} does NOT exist in frame.'
+    assert time_col in long_df.columns, f"{time_col} does NOT exist in frame."
 
     # make sure data is sorted in time order
     if sort_time_col:
@@ -99,9 +145,9 @@ def long_format_ts_embargo_split(long_df: pd.DataFrame,
         train_time, test_time = X[train].squeeze(), X[test].squeeze()
 
         if debug:
-            print(f'train size = {len(train)}, test size = {len(test)}')
-            print(f'train time tail = {train_time[-5:]}')
-            print(f'test time head = {test_time[:5]}')
+            print(f"train size = {len(train)}, test size = {len(test)}")
+            print(f"train time tail = {train_time[-5:]}")
+            print(f"test time head = {test_time[:5]}")
 
         # if i don't do .values below, the reuslts are all False.
         # this is because numpy and pandas use different datetime types...
